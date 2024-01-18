e5c570e56fe753925072fcbcbe3129052c17fc21
bielalmd <bielalmd@gmail.com>
2024-01-17 23:35:32 -0300

    feat: new component

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -1,11 +1,15 @@
+// Components
+import FirstComponent from './components/FirstComponent';
+
+// Styles
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <h2>Emmet it work</h2>
-      <p>Lorem ipsum dolor sit amet.</p>
+      <FirstComponent/>
+      
     </div>
   );
 }
