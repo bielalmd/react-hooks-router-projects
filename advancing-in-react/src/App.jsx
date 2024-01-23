a4bed902141a20e6ee5b1a24f28ad23482d8dc27
bielalmd <bielalmd@gmail.com>
2024-01-23 19:42:44 -0300

    feat: adding images on app.jsx

--- advancing-in-react/src/App.jsx
+++ advancing-in-react/src/App.jsx
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <h1>The new Section, the third</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem com pôr do sol e arvores" />
+      </div>
     </div>
   );
 }
