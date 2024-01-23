773599ca62ec1861f07fc10a5c7dac3925b5a2d4
bielalmd <bielalmd@gmail.com>
2024-01-22 23:44:30 -0300

    feat: more about events and functions

--- hello-world/src/components/Events.jsx
+++ hello-world/src/components/Events.jsx
@@ -6,9 +6,28 @@ const Events = () => {
         console.log('your button it working');
     };
 
+    const render = (x) => {
+
+        if(x) {
+            return <h1> Render this!</h1>
+        } else {
+            return <h1>I can render this too</h1>
+        }
+    }
+
     return (
         <div>
             <button onClick={handleMyEvent}>Click here</button>
+            <div>
+                <button onClick={() => console.log('clicou')}>Click here too</button>
+                <button onClick={() => {
+                    if(true) {
+                        console.log("this shouldnt exist =(");
+                    }
+                }}>Please click here</button>
+            </div>
+            {render(true)}
+            {render(false)}
         </div>
     )
 }
